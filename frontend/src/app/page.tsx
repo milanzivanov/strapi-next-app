5da17b499ea4a394d96fc8f1082eed71af45adb4
milan <milanzivanov@users.noreply.github.com>
2025-11-10 12:28:54 +0100

    Epic Next.js 15 Tutorial Part 2: Building Out The Home Page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,33 @@
-async function getStrapiData(url: string) {
+import { HeroSection } from "@/components/custom/hero-section";
+import qs from "qs";
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      on: {
+        "layout.hero-section": {
+          populate: {
+            image: {
+              fields: ["url", "alternativeText"]
+            },
+            link: {
+              populate: true
+            }
+          }
+        }
+      }
+    }
+  }
+});
+
+async function getStrapiData(path: string) {
   const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
   try {
-    const response = await fetch(baseUrl + url);
+    const response = await fetch(url.href);
     if (!response.ok) {
       throw new Error("Failed to fetch data from Strapi");
     }
@@ -15,14 +41,13 @@ async function getStrapiData(url: string) {
 export default async function Home() {
   const strapiData = await getStrapiData("/api/home-page");
 
-  console.log("Strapi Data:", strapiData);
+  console.dir(strapiData.data, { depth: null });
 
-  const { title, description } = strapiData.data;
+  const { blocks } = strapiData.data;
 
   return (
     <main className="container mx-auto py-6">
-      <h1 className="text-5xl font-bold">{title}</h1>
-      <p className="text-xl mt-4">{description}</p>
+      <HeroSection data={blocks[0]} />
     </main>
   );
 }
